ec9a71ac780af213902236ca0c6a44d8e3d8dbbb
Michael Irwin <mike.irwin1@gmail.com>
2018-12-07 13:07:55 +0000

    initializing complete

--- models/customer.rb
+++ models/customer.rb
@@ -1,3 +1,16 @@
 require_relative('../db/sql_runner')
 require_relative('film')
 require_relative('ticket')
+
+class Customer
+
+  attr_accessor :name, :funds
+  attr_reader :id
+
+  def initialize(options)
+    @id = options['id'].to_i if options['id']
+    @name = options['name']
+    @funds = options['funds']
+  end
+
+end
--- models/film.rb
+++ models/film.rb
@@ -1,3 +1,16 @@
 require_relative('../db/sql_runner')
 require_relative('customer')
 require_relative('ticket')
+
+class Film
+
+  attr_accessor :title, :price
+  attr_reader :id
+
+  def initialize(options)
+    @id = options['id'].to_i if options['id']
+    @title = options['title']
+    @price = options['price']
+  end
+
+end
