155c8553370108cf2ea913cba7d6b3dc5dc6e429
Michael Irwin <mike.irwin1@gmail.com>
2018-12-07 13:29:06 +0000

    customers individual functions complete

--- console.rb
+++ console.rb
@@ -3,7 +3,17 @@ require_relative('models/customer')
 require_relative('models/film')
 require_relative('models/ticket')
 
+Customer.delete_all
+
 customer1 = Customer.new({'name' => 'Jeff Beck', 'funds' => 10})
 customer2 = Customer.new({'name' => 'Joe Satriani', 'funds' => 20})
 customer1.save
 customer2.save
+
+customer1.funds = 15
+customer1.update()
+
+p Customer.all
+
+binding.pry
+nil
--- models/customer.rb
+++ models/customer.rb
@@ -23,4 +23,22 @@ class Customer
     sql = "DELETE FROM customers"
     SqlRunner.run(sql)
   end
+
+  def delete()
+    sql = "DELETE FROM customers WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
+
+  def update()
+    sql = "UPDATE customers SET (name, funds) = ($1,$2) WHERE id = $3"
+    values = [@name, @funds, @id]
+    SqlRunner.run(sql, values)
+  end
+
+  def self.all()
+    sql = "SELECT * FROM customers"
+    customers = SqlRunner.run(sql)
+    return customers.map {|customer| Customer.new(customer)}
+  end
 end
