a66da2cf5f1cf58334ddc223d8b4336d93315ff7
Michael Irwin <mike.irwin1@gmail.com>
2018-12-07 11:57:00 +0000

    require relatives set-up

---
+++ models/customer.rb
@@ -0,0 +1,3 @@
+require_relative('../db/sql_runner')
+require_relative('film')
+require_relative('ticket')
---
+++ models/ticket.rb
@@ -0,0 +1,3 @@
+require_relative('../db/sql_runner')
+require_relative('customer')
+require_relative('film')
