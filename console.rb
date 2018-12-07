9f23ca76a6b623e625a8ab85cd16fc278521d09c
Michael Irwin <mike.irwin1@gmail.com>
2018-12-07 14:08:14 +0000

    tickets save and delete added

--- console.rb
+++ console.rb
@@ -16,6 +16,12 @@ film1.save
 film2 = Film.new({'title' => 'Blade Runner', 'price' => 2})
 film2.save
 
+ticket1 = Ticket.new({'customer_id' => customer1.id, 'film_id' => film1.id})
+ticket2 = Ticket.new({'customer_id' => customer2.id, 'film_id' => film2.id})
+ticket3 = Ticket.new({'customer_id' => customer1.id, 'film_id' => film2.id})
+ticket1.save
+ticket2.save
+ticket3.save
 
 binding.pry
 nil
--- models/ticket.rb
+++ models/ticket.rb
@@ -13,4 +13,16 @@ class Ticket
     @film_id = options['film_id'].to_i
   end
 
+  def save()
+    sql = "INSERT INTO tickets (customer_id, film_id) VALUES ($1, $2) RETURNING id"
+    values = [@customer_id, @film_id]
+    @id = SqlRunner.run(sql, values)[0]['id'].to_i
+  end
+
+  def delete()
+    sql = "DELETE FROM tickets WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
+
 end
